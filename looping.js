26f03140594ea7056b80532708cc9fece3f9b20d
yayang <yayangikbal99@gmail.com>
2019-07-14 14:05:57 +0700

    tambahn 1

---
+++ looping.js
@@ -0,0 +1,37 @@
+var names = ["Fardan", "Ibnu", "Ega", "Asep"];
+var peserta = [
+
+
+
+]
+
+
+//1. While
+var i = 0;
+while(i < names.length) {
+    console.log(names[i]);
+    i++;
+
+}
+
+//operator == membandingkan isi saja
+//operator === membandingkan isi dan tipe data
+
+//2. Do While
+var x = 0;
+do {
+    console.log('Do While', x);
+    x++;
+} while(x < 1);
+
+//Dikerjain gaes
+//1. Buat masing-masing 2 variable untuk tipe data Boolean, Array, Object dan Array of Object
+//2. Buat code looping dengan menggunakan Array of Object (Bebas pake while atau do while)
+    [
+        {
+            name,
+            age,
+            gender
+        }
+    ]
+    di dalam fungsi loopingnya, tampilkan dalam 1 baris "Fardan - Umur 20 - Laki-laki"
