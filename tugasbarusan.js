1e1687bc892c07c28b43ca3328a151e23e05059c
yayang <yayangikbal99@gmail.com>
2019-08-25 12:34:18 +0700

    tugas nh

---
+++ tugasbarusan.js
@@ -0,0 +1,9 @@
+
+
+if(1 != 1){
+    console.log('[IF:ELSE IF]1 tidak sama dengan 1');
+}else if (1 > 1) {
+    console.log('[IF ELSE IF]tapi 1 lebih dari 0');
+}else {
+    console.log('[IF ELSE IF]ambil semua bro');
+}
